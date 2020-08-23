3f8cd3a6f73aa23d185848d05a5ee160bf08c1ec
中山優 <croixdusudx@gmail.com>
2020-08-23 17:17:32 +0900

    Firebase認証状態に応じたページ遷移の実装

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Top from './pages/Top';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Auth from './components/Auth';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Auth>
+          <Switch>
+            <Route exact path="/" component={Top} />
+          </Switch>
+        </Auth>
+      </Switch>
+    </Router>
   );
 }
 
